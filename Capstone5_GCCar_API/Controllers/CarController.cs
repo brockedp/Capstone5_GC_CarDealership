ec7459b9a5d325fd8f79544198c4240cd4f27e24
Deno <denobrockert@yahoo.com>
2019-09-03 18:44:40 -0400

    First push of api

---
+++ Capstone5_GCCar_API/Controllers/CarController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capstone5_GCCar_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Capstone5_GCCar_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : Controller
+    {
+        private readonly CarDbContext _context;
+        public CarController(CarDbContext context)
+        {
+            _context = context;
+
+            if(_context.Car.Count() == 0)
+            {
+                _context.Car.Add(new Car { Make = "Ford", Model = "Focus", Year = 1996, Color = "Black" });
+            }
+        }
+        // Get: api/Car/
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Car>>> GetCars()
+        {
+            var carList = await _context.Car.ToListAsync();
+            return carList;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Car>> GetCarById(int id)
+        {
+            var car = await _context.Car.FindAsync(id);
+            if(car == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return car;
+            }
+        }
+        [HttpPost]
+        public async Task<ActionResult<Car>> PostCar(Car newCar)
+        {
+            _context.Car.Add(newCar);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetCarById),new { id = newCar.Id },newCar);
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Car>> PutCar(int id, Car updateCar)
+        {
+            if (id != updateCar.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(updateCar).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Car>> DeleteCar(int id)
+        {
+            var car = await _context.Car.FindAsync(id);
+            if (car != null)
+            {
+                return NotFound();
+            }
+            _context.Car.Remove(car);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ Capstone5_GCCar_API/Models/CarDbContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Capstone5_GCCar_API.Models
+{
+    public partial class CarDbContext : DbContext
+    {
+        public CarDbContext()
+        {
+        }
+
+        public CarDbContext(DbContextOptions<CarDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Car> Car { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=.\\sqlexpress;Database=CarDb;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Car>(entity =>
+            {
+                entity.Property(e => e.Color).HasMaxLength(40);
+
+                entity.Property(e => e.Make).HasMaxLength(40);
+
+                entity.Property(e => e.Model).HasMaxLength(40);
+            });
+        }
+    }
+}
